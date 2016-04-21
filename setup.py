5229f3b4607a5b0f4a888ada35779b8bc963e148
Jan Niklas Hasse <jhasse@bixense.com>
2016-04-21 02:20:42 +0200

    Add setuptools tools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='minclude',
+    version='1.0.0',
+    author="Jan Niklas Hasse",
+    author_email="jhasse@bixense.com",
+    url="https://github.com/jhasse/minclude",
+    description="Remove unnecessary include directives in C/C++ projects",
+    scripts=['minclude'],
+)
