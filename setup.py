b05a48cf79e3d62dddf0069522e0c168bfdf6869
Jan Niklas Hasse <jhasse@bixense.com>
2017-03-29 13:44:14 +0200

    Add download_url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="Jan Niklas Hasse",
     author_email="jhasse@bixense.com",
     url="https://github.com/jhasse/minclude",
+    download_url='https://github.com/jhasse/minclude/archive/v1.0.0.tar.gz',
     description="Remove unnecessary include directives in C/C++ projects",
     scripts=['minclude'],
 )
