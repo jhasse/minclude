c283e706dfa1b36c3c1db9e48caf2c40783b07b0
Jan Niklas Hasse <jhasse@bixense.com>
2017-11-02 15:07:16 +0100

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='minclude',
-    version='1.0.0',
+    version='1.1.0',
     author="Jan Niklas Hasse",
     author_email="jhasse@bixense.com",
     url="https://github.com/jhasse/minclude",
