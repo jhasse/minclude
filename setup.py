619c10afb9ac85df946ce91831f216ea425e8a0f
Jan Niklas Hasse <jhasse@bixense.com>
2025-02-18 21:08:24 +0100

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='minclude',
-    version='1.0.0',
+    version='1.2.0',
     author="Jan Niklas Hasse",
     author_email="jhasse@bixense.com",
     url="https://github.com/jhasse/minclude",
-    download_url='https://github.com/jhasse/minclude/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/jhasse/minclude/archive/v1.2.0.tar.gz',
     description="Remove unnecessary include directives in C/C++ projects",
     scripts=['minclude'],
     install_requires=[
